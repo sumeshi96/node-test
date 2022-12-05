fdba8c22f1e84379966c084f018f488e29ab6a6b
sumeshi96 <yumapc96@gmail.com>
2022-12-06 03:04:15 +0900

    update: dbへの接続をするコードを書いた

--- node/workspace/index.js
+++ node/workspace/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const mysql = require('mysql')
 const app = express()
 const port = 3000
+
 
 app.get('/', (req, res) => {
     res.send("Hello World!")
@@ -8,4 +10,19 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
+
+const connection = mysql.createConnection({
+    host: 'localhost', //localhostはコンテナのIPを指定する
+    user: 'docker',
+    password: 'password',
+    database: 'sample'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('success');
+});
